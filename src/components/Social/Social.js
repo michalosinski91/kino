29c87e4e633c22fbd616ba8a561311be1b1bf65b
michalosinski91 <mosinski@hotmail.co.uk>
2020-03-26 14:11:59 +0100

    Social section styled

--- src/components/Social/Social.js
+++ src/components/Social/Social.js
@@ -4,10 +4,24 @@ import "./Social.scss";
 export default function Social() {
   return (
     <ul className="social-list">
-      <li className="social-list__item">icon</li>
-      <li className="social-list__item">icon</li>
-      <li className="social-list__item">icon</li>
-      <li className="social-list__item">English</li>
+      <li className="social-list__item">
+        <a href="#">
+          <i className="fab fa-facebook-f"></i>
+        </a>
+      </li>
+      <li className="social-list__item">
+        <a href="#">
+          <i className="fab fa-instagram"></i>
+        </a>
+      </li>
+      <li className="social-list__item">
+        <a href="#">
+          <i className="fab fa-youtube"></i>
+        </a>
+      </li>
+      <li className="social-list__item">
+        <a href="#">English</a>
+      </li>
     </ul>
   );
 }
