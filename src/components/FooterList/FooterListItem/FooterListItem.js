f4d1398c679f7239e4c9c1265b5ad5a211958244
michalosinski91 <mosinski@hotmail.co.uk>
2020-03-26 14:36:40 +0100

    FooterList styled

--- src/components/FooterList/FooterListItem/FooterListItem.js
+++ src/components/FooterList/FooterListItem/FooterListItem.js
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function FooterListItem({ section }) {
-  return <li className="footer-list__item">{section}</li>;
+  return (
+    <li className="footer-list__item">
+      <a href="#">{section}</a>
+    </li>
+  );
 }
