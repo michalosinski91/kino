5c8f08ce2d72c639f85b53c69bd7900323a31d4f
michalosinski91 <mosinski@hotmail.co.uk>
2020-03-26 13:53:10 +0100

    Copyright section styling

--- src/components/Copyright/Copyright.js
+++ src/components/Copyright/Copyright.js
@@ -2,5 +2,18 @@ import React from "react";
 import "./Copyright.scss";
 
 export default function Copyright() {
-  return <div className="copyright">Copyright</div>;
+  return (
+    <div className="copyright">
+      <p>
+        Built by{" "}
+        <a href="https://www.michalosinski.dev" target="blank">
+          Michał Osiński
+        </a>
+        . Inspiration taken from{" "}
+        <a href="https://nowyteatr.org/pl" target="blank">
+          nowyteatr.org
+        </a>
+      </p>
+    </div>
+  );
 }
