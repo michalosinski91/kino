d35d920b85b6e3880ac23bab17e5c865e577a71a
michalosinski91 <mosinski@hotmail.co.uk>
2020-03-26 16:18:06 +0100

    Logo styled

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -2,5 +2,11 @@ import React from "react";
 import "./Logo.scss";
 
 export default function Logo() {
-  return <div className="logo">Logo</div>;
+  return (
+    <div className="logo">
+      <a href="#">
+        <span>Kino Noir</span>
+      </a>
+    </div>
+  );
 }
