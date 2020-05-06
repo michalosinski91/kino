9edb340b5059246a12f12007c70d26f6dd87d342
michalosinski91 <mosinski@hotmail.co.uk>
2020-05-06 14:41:23 +0200

    Filter component added & styled

---
+++ src/components/Filter/Filter.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./Filter.scss";
+
+export default function Filter() {
+  return (
+    <div className="filter">
+      <div className="filter__container">
+        <h3 className="heading heading--tertiary">Gatunek:</h3>
+        <li className="filter__checklist">
+          <button className="filter__checklist-button">Komedia</button>
+          <button className="filter__checklist-button filter__checklist-button--checked">
+            Dramat
+          </button>
+          <button className="filter__checklist-button">Akcja</button>
+          <button className="filter__checklist-button">Thriller</button>
+          <button className="filter__checklist-button">Animacja</button>
+          <button className="filter__checklist-button">Romans</button>
+          <button className="filter__checklist-button">Horror</button>
+        </li>
+      </div>
+      <div className="filter__container">
+        <h3 className="heading heading--tertiary">Wiek:</h3>
+        <li className="filter__checklist">
+          <button className="filter__checklist-button">Familijny</button>
+          <button className="filter__checklist-button filter__checklist-button--checked">
+            12+
+          </button>
+          <button className="filter__checklist-button">15+</button>
+          <button className="filter__checklist-button">18+</button>
+        </li>
+      </div>
+    </div>
+  );
+}
--- src/pages/Calendar/Calendar.js
+++ src/pages/Calendar/Calendar.js
@@ -2,11 +2,13 @@ import React from "react";
 import "./Calendar.scss";
 import CalendarHeader from "../../components/CalendarHeader/CalendarHeader";
 import CalendarSchedule from "../../components/CalendarSchedule/CalendarSchedule";
+import Filter from "../../components/Filter/Filter";
 
 export default function Calendar() {
   return (
     <section data-testid="calendar" className="calendar">
       <CalendarHeader />
+      <Filter />
       <CalendarSchedule />
     </section>
   );
